b005e470d857ae64d363b480d07838b3e1d902cb
tuan <rickytuan1991@gmail.com>
2022-10-02 22:45:42 +0300

    done

--- app.js
+++ app.js
@@ -6,3 +6,13 @@ const result = _.add(1, 1)
 // console.log(result)
 const newItem = _.flattenDeep(items)
 console.log(newItem)
+
+console.log('1 task')
+setTimeout(() => {
+    console.log('2 task')
+}, 0);
+console.log('3 task')
+
+
+
+console.log('this 444')
