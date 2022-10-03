06f4d6d8f274bf3826e53ede00c4458f5f8a528e
tuan <rickytuan1991@gmail.com>
2022-10-03 07:47:36 +0300

    done

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
-setInterval(() => {
-    console.log('interval')
-}, (2000));
-console.log('i will run first')
+const {createServer} = require('http')
+
+createServer((request,result)=> { 
+    console.log('request event')
+    result.write('url: ' + request.url)
+    result.end('hello world')
+
+
+}).listen(5000,()=> { 
+    console.log('server on listening  on port : 5000.... ')
+})
+
