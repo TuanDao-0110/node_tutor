02d6c5259928065c196ce1a974f2b4ac7706cb21
tuan <rickytuan1991@gmail.com>
2022-10-06 22:47:42 +0300

    events

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
-const {createServer} = require('http')
+const EventEmiter = require('events')
+const customEmitter = new EventEmiter()
 
-createServer((request,result)=> { 
-    console.log('request event')
-    result.write('url: ' + request.url)
-    result.end('hello world')
-
-
-}).listen(5000,()=> { 
-    console.log('server on listening  on port : 5000.... ')
+// we have an event on which name is 'response' ==> when the event take place ==> the call back function will be executed. 
+customEmitter.on('response',()=> { 
+    console.log(`data received`)
 })
+customEmitter.on('no',()=> { 
+    console.log('something')
+})
+// when the we emit the event name 'response' ==>  
+customEmitter.emit('response')
+customEmitter.emit('no')
+
+
+
 
