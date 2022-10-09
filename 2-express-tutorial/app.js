580703cb2e8eefe687a789826e12732e7138e821
tuan <rickytuan1991@gmail.com>
2022-10-09 22:46:13 +0300

    express http basic

--- 2-express-tutorial/app.js
+++ 2-express-tutorial/app.js
@@ -3,8 +3,7 @@ const http = require('http')
 
 // when create server callback function
 const server = http.createServer((request, response) => {
-    console.log('user hit server')
-
+    response.end('welcome to our home page')
 })
 
 server.listen(4000, () => {
