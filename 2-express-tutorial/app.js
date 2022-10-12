bf78fed3cd7b5d8d40d54a518dceb70ad4977b1a
tuan <rickytuan1991@gmail.com>
2022-10-12 21:44:14 +0300

    use express

--- 2-express-tutorial/app.js
+++ 2-express-tutorial/app.js
@@ -1,18 +1,12 @@
 const express = require('express')
 const app = express()
 
-
+const logger = require('./logger')
 
 // req => middleware => res
-const logger =(req,res,next)=> { 
-    const { method, url } = req
-    const time = new Date().getFullYear()
-    console.log(method, url, time)
-    res.send('testing')
-    next() 
-}
+app.use(logger)
 
-app.get('/',logger,(req,res)=> { 
+app.get('/',(req,res)=> { 
 
     res.send('Home')
 })
@@ -22,6 +16,13 @@ app.get('/about', (req, res) => {
     res.send('About')
 })
 
+app.get('/api/products',(req,res)=> { 
+    res.send('Product')
+})
+
+app.get('/api/items', (req, res) => {
+    res.send('items')
+})
 app.listen(4000,()=> { 
     console.log("listening on port 4000 ....")
 })
