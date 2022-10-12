d88523dd83ce360f86fc858ebc5d147afebf8ecf
tuan <rickytuan1991@gmail.com>
2022-10-12 21:26:47 +0300

    next () in express

--- 2-express-tutorial/app.js
+++ 2-express-tutorial/app.js
@@ -1,9 +1,26 @@
 const express = require('express')
 const app = express()
+
+
+
 // req => middleware => res
+const logger =(req,res,next)=> { 
+    const { method, url } = req
+    const time = new Date().getFullYear()
+    console.log(method, url, time)
+    res.send('testing')
+    next() 
+}
 
+app.get('/',logger,(req,res)=> { 
 
+    res.send('Home')
+})
 
+app.get('/about', (req, res) => {
+
+    res.send('About')
+})
 
 app.listen(4000,()=> { 
     console.log("listening on port 4000 ....")
