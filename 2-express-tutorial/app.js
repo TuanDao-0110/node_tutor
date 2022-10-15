10e36f9b37f9a4814562244ab582d19153961e77
tuan <rickytuan1991@gmail.com>
2022-10-15 04:40:36 +0300

    delete method

--- 2-express-tutorial/app.js
+++ 2-express-tutorial/app.js
@@ -49,7 +49,46 @@ app.post('/api/people', (req, res) => {
 })
 
 
+
+app.post('/api/postman/people', (req, res) => {
+    const { name } = req.body
+    if (!name) {
+        return res.status(400).json({ success: false, msg: "please provide name" })
+    }
+    res.status(201).send({ success: 'success', data: [...people, name] })
+})
 // 3. PUT method
+
+app.put('/api/postman/people/:id', (req, res) => {
+    // 1. get id from params
+    const { id } = req.params
+    // 2. put the new name to data
+    const { name } = req.body
+    // 3. now check it:
+    const personIndex = people.findIndex((person) => person.id === Number(id))
+    if (personIndex !== -1) {
+        people[personIndex].name = name
+        return res.status(200).json({ success: true, data: people })
+    }
+    return res.status(400).json({ success: false, msg: `can not find matching person with id : ${id}` })
+
+})
+
+// 4. delete:
+
+
+app.delete('/api/postman/people/:id', (req, res) => {
+    // 1. get the id ==> 
+    const { id } = req.params
+    const personIndex = people.findIndex(person => person.id === Number(id))
+    if (personIndex > -1) {
+        // console.log(people.splice(personIndex, 1))
+        // console.log(people)
+        return res.status(200).json({ success: true, data: people })
+    }
+    return res.status(400).send({ success: false, msg: `can not find matching person with id : ${id}` })
+
+})
 
 
 app.get('*', (req, res) => {
