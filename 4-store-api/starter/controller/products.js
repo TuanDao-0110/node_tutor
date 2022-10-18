f720145754d1ebb8f3ca3374f8fa9ebbadff2228
tuan <rickytuan1991@gmail.com>
2022-10-18 20:33:17 +0300

    object

--- 4-store-api/starter/controller/products.js
+++ 4-store-api/starter/controller/products.js
@@ -3,18 +3,41 @@ const Product = require('../model/product')
 const getAllProductStatic = async (req, res, next) => {
 
     const products = await Product.find({
-        featured: 'not'
+        // name: "2",
     })
-    res.status(200).json({ products })
-  
+    res.status(200).json({ products, nbHits: products.length })
+
     // if (!products) throw Error("access denied");
 
 }
 
 const getAllProduct = async (req, res) => {
+    // console.log(req.query)
+    // 1. get feature on query ==> to get value from database. 
+    const { featured, company, name } = req.query
 
-    res.status(200).json({ msg: 'product testing' })
+    const queryObject = {}
+    // 2 check query 
+    if (featured) {
+        queryObject.featured = featured === 'true' ? true : false
+    }
+    if (company) {
+        queryObject.company = company
+    }
+    if (name) {
+        queryObject.name = name
+    }
+    console.log(queryObject)
+    const product = await Product.find(queryObject)
+    res.status(200).json({ msg: product, nbHits: product.length })
+}
+
+// 1 get each product
+const getProduct = async (req, res, next) => {
+    console.log(req.query)
+    const product = await Product.find(req.query)
+    res.status(200).json({ msg: product })
 }
 
 
-module.exports = { getAllProduct, getAllProductStatic }
+module.exports = { getAllProduct, getAllProductStatic, getProduct }
--- 4-store-api/starter/router/router.js
+++ 4-store-api/starter/router/router.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const router = express.Router()
-const { getAllProduct, getAllProductStatic } = require('../controller/products')
+const { getAllProduct, getAllProductStatic, getProduct } = require('../controller/products')
 
-router.route('/').get(getAllProduct)
+router.route('/').get(getAllProduct).get(getProduct)
 router.route('/static').get(getAllProductStatic)
+
 module.exports = router
